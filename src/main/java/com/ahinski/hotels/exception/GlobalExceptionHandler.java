abcb2f3f2385562997ba6af8b63b789471122bca
DimochKaaa <zaaz288@mail.ru>
2025-02-09 07:21:03 +0300

    Added GlobalExceptionHandler

---
+++ src/main/java/com/ahinski/hotels/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.ahinski.hotels.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler
+    public ResponseEntity<String> handleValidationExceptions(final ValidationException validationException) {
+        return new ResponseEntity<>(validationException.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
