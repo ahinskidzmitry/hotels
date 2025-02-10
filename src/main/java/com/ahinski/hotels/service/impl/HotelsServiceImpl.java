3b52dbfcaf18d5301733208d374529d8bb7cb237
DimochKaaa <zaaz288@mail.ru>
2025-02-10 23:59:03 +0300

    Fixed extra import

--- src/main/java/com/ahinski/hotels/service/impl/HotelsServiceImpl.java
+++ src/main/java/com/ahinski/hotels/service/impl/HotelsServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 import com.ahinski.hotels.converter.DtoConverter;
 import com.ahinski.hotels.converter.EntityToDtoConverter;
